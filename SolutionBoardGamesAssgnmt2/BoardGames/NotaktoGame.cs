e90506792a0da7fbdd9043a2a7b3e79513ae399d
Luis Fernando Sanchez <luisfer170399@gmail.com>
2025-05-05 17:20:18 +1000

    Notakto Computer now plays to win

--- SolutionBoardGamesAssgnmt2/BoardGames/NotaktoGame.cs
+++ SolutionBoardGamesAssgnmt2/BoardGames/NotaktoGame.cs
@@ -403,35 +403,68 @@ namespace BoardGames
 
         public NotaktoComputer(string name) : base(name) { }  // pass name up
 
-        public override (int board, int row, int col) MakeMove( List<Board> boards, HashSet<int> deadBoards)
+        public override (int board, int row, int col) MakeMove(
+    List<Board> boards, HashSet<int> deadBoards)
+{
+    // Gather all possible moves on active boards
+    var options = new List<(int board, int row, int col)>();
+    for (int b = 0; b < boards.Count; b++)
+    {
+        if (deadBoards.Contains(b)) continue;
+        for (int i = 0; i < boards[b].Size; i++)
+            for (int j = 0; j < boards[b].Size; j++)
+                if (boards[b].Cells[i][j] == null)
+                    options.Add((b, i, j));
+    }
+
+    // Classify moves into those that kill a board vs those that don't
+    var killMoves = new List<(int board, int row, int col)>(); // moves that complete a three-in-a-row
+    var safeMoves = new List<(int board, int row, int col)>(); // moves that do not
+
+    int live = boards.Count - deadBoards.Count;
+
+    foreach (var opt in options)
+    {
+        var (b, r, c) = opt;
+        // simulate placing an X
+        boards[b].Cells[r][c] = 1;
+        bool kills = NotaktoGame.HasThreeInARowStatic(boards[b]);
+        // revert simulation
+        boards[b].Cells[r][c] = null;
+
+        if (kills)
         {
-            var options = new List<(int board, int row, int col)>();  // possible moves
-            for (int b = 0; b < boards.Count; b++)
-            {
-                if (deadBoards.Contains(b)) continue;  // skip dead boards
-                for (int i = 0; i < boards[b].Size; i++)
-                    for (int j = 0; j < boards[b].Size; j++)
-                        if (boards[b].Cells[i][j] == null)
-                            options.Add((b, i, j));  // add empty cell
-            }
-
-            if (options.Count == 0)
-                throw new Exception("No valid moves left.");  // should never happen
-
-            // prefer moves that don't kill the last board
-            foreach (var opt in options)
-            {
-                var (b, r, c) = opt;
-                boards[b].Cells[r][c] = 1;
-                bool killsLast = (deadBoards.Count
-                                    + (NotaktoGame.HasThreeInARowStatic(boards[b]) ? 1 : 0))
-                                    == boards.Count;
-                boards[b].Cells[r][c] = null;
-                if (!killsLast) return opt;  // choose safe move
-            }
-
-            return options[rand.Next(options.Count)];  // random fallback
+            // only consider killing if it won't be the last board
+            if (deadBoards.Count + 1 < boards.Count)
+                killMoves.Add(opt);
         }
+        else
+        {
+            safeMoves.Add(opt);
+        }
+    }
+
+    // 1. Prefer kill moves that leave an even number of boards (parity strategy)
+    if (killMoves.Count > 0)
+    {
+        var goodKills = killMoves
+            .Where(m => ((live - 1) % 2) == 0)
+            .ToList();
+        if (goodKills.Count > 0)
+            return goodKills[rand.Next(goodKills.Count)];
+        // if none maintain parity, pick any kill move
+        return killMoves[rand.Next(killMoves.Count)];
+    }
+
+    // 2. If no kill move, pick a safe move at random
+    if (safeMoves.Count > 0)
+        return safeMoves[rand.Next(safeMoves.Count)];
+
+    // 3. Fallback (should never happen): pick any available move
+    return options[rand.Next(options.Count)];
+}
+
+
 
 
     } // End of NotaktoComputer class
